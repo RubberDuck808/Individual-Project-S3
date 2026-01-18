e8ebc059ad18baae26343683f25dc13d69af8eba
I54789 <o.rashid@student.fontys.nl>
2026-01-18 17:05:44 +0100

    User location fix, inside of app jsx and main jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,18 @@
-// Using 'buffer' package polyfill for browser compatibility (not Node.js built-in 'node:buffer')
-import { Buffer } from "buffer"; // NOSONAR
+import { Buffer } from "buffer";
 globalThis.Buffer = Buffer;
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
